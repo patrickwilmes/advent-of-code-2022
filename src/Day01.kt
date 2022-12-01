aa7634d93fee0ca6171341d186e33684d104bb28
Patrick Wilmes <patrick.wilmes@bit-lake.com>
2022-12-01 13:15:51 +0100

    Day one challenge solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
+typealias ElvesWithTotalCalories = List<List<Int>>
 fun main() {
+    val calculateCaloriesCarriedByAllElves: (List<String>) -> ElvesWithTotalCalories = {
+        it.fold(mutableListOf(mutableListOf<Int>())) { acc, item ->
+            if (item.isBlank()) {
+                acc.add(mutableListOf())
+            } else {
+                acc.last().add(item.toInt())
+            }
+            acc
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elvesCalories = calculateCaloriesCarriedByAllElves(input)
+        return elvesCalories.maxOfOrNull {
+            it.sum()
+        } ?: 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return calculateCaloriesCarriedByAllElves(input)
+            .map { it.sum() }
+            .sortedDescending()
+            .subList(0, 3)
+            .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
